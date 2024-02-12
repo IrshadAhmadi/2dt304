472ef44e248655cb02ba6dfc7b5b22dcc2c6ea96
Irshad Ahmadi <ia222ki@student.lnu.se>
2024-02-12 23:35:21 +0100

    login + login.css

--- src/back/routes/users.js
+++ src/back/routes/users.js
@@ -7,7 +7,9 @@ router.post('/register', userController.registerUser)
 router.get('/register', (req, res) => {
   res.render('register')
 })
-router.get('/login', (req, res) => res.render('login'))
+router.get('/login', (req, res) => {
+  res.render('login', { user: req.user })
+})
 router.post('/login', userController.loginUser)
 
 router.get('/logout', userController.logoutUser)
